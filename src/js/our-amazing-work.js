96cb25988da0553a5a3c220ffcba4c53bbaa3700
Oleg <tytarenkooleg@gmail.com>
2018-11-27 22:32:49 +0200

    ver 1.25 (correct "our amazing work")

--- src/js/our-amazing-work.js
+++ src/js/our-amazing-work.js
@@ -1,19 +1,19 @@
-$(document).ready(function () {
-    $(".our_amazing_work_images .our_amazing_work_img-item").slice(0, 12).show();
-    $('.links.portfolio-category').click(function () {
-        let category = $(this).attr("data-info");
-        $(".our_amazing_work_img-item").hide();
-        $(".our_amazing_work_img-item[data-info=" + category + "]").show();
-        if (category == 'all_images') {
-            $(".our_amazing_work_images .our_amazing_work_img-item").slice(0, 36).show();
-        }
-    });
-
-    $(".btn_with_plus").on('click', function (e) {
-        e.preventDefault();
-        $(".our_amazing_work_images .our_amazing_work_img-item:hidden").slice(0, 12).slideDown();
-        if ($(".our_amazing_work_images .our_amazing_work_img-item:hidden").length == 0) {
-            $(".btn_with_plus").fadeOut('slow');
-        }
-    });
-});
+// $(document).ready(function () {
+//     $(".our-amazing-work__block-images .our-amazing-work__image").slice(0, 12).show();
+//     $(".links .portfolio-category").click(function () {
+//         let categ = $(this).attr("data-info");
+//         $(".our-amazing-work__image").hide();
+//         $(".our-amazing-work__image[data-info=" + categ + "]").show();
+//         if (categ == 'all-images') {
+//             $(".our-amazing-work__block-images .our-amazing-work__image").slice(0, 36).show();
+//         }
+//     });
+//
+//     $(".btn_with_plus").on('click', function (e) {
+//         e.preventDefault();
+//         $(".our-amazing-work__block-images .our-amazing-work__image:hidden").slice(0, 12).slideDown();
+//         if ($(".our-amazing-work__block-images .our-amazing-work__image:hidden").length == 0) {
+//             $(".btn_with_plus").fadeOut('slow');
+//         }
+//     });
+// });
